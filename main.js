478015da9dfed13e96df8d1c7ef5050d839c5f3a
C-Hindustries <c-h-F-15@kcs.org.uk>
2016-06-20 19:57:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-
+function startGame() {
+  var gameStartPrompt = prompt("Welcome! Please Choose a Name For Your ship!");
+  rocketName = gameStartPrompt;
+  document.getElementById("rocketName").innerHTML = rocketName;
+  document.getElementById("starterDiv").className = "hidden";
+  document.getElementById("statsDiv").className = "statsDiv";
+}
